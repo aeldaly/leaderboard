d2df940b8f1cc085f429aa77e5919279288a7a1e
Ahmed El-Daly <aeldaly@gmail.com>
2017-03-12 18:30:18 -0400

    Game model/controller all set

---
+++ app/models/concerns/game_validator.rb
@@ -0,0 +1,7 @@
+class GameValidator < ActiveModel::Validator
+  def validate(record)
+    if record.winner_score < record.loser_score + 2
+      record.errors[:winner] << 'must win by a margin of 2 or more'
+    end
+  end
+end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,10 @@
 class Game < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with GameValidator
+
   belongs_to :winner, class_name: User
   belongs_to :loser, class_name: User
 
   validates :winner, :loser, :date, :winner_score, :loser_score, presence: true
+  validates :winner_score, numericality: { greater_than_or_equal_to: 21 }
 end
